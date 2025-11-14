b066571f4837d869cfd6232e893335fa61df1bb5
Christy Serna <christyserna04@gmail.com>
2025-11-14 17:48:50 -0600

    UNFINISHED question7.c

---
+++ question7.c
@@ -0,0 +1,3 @@
+// write a program that computes area and circumference of a circle
+// using both the #define and const for PI
+// user must input radius, and the output mus be formatted to two decimals
