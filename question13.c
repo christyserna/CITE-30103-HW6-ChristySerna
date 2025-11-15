2d6f79442a966f4dbb5f13e1248f872723c9ec08
Christy Serna <christyserna04@gmail.com>
2025-11-14 18:37:09 -0600

    UNIFNISHED question13.c

---
+++ question13.c
@@ -0,0 +1,20 @@
+// write a program that checks whether a number is divisible by both 2 & 3, but not 7
+// using logical operators only
+
+#include <stdio.h>
+
+int main() {
+
+  int a, b, c;
+
+  printf("insert an integer(a): ");
+  scanf("%d", &a);
+
+  printf("insert an integer(b): ");
+  scanf("%d", &b);
+
+  
+
+  
+return 0;
+}
