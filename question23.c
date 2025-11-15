c97c35a28ca4fff3c14fe088feaeef4a32d015cd
Christy Serna <christyserna04@gmail.com>
2025-11-14 22:16:55 -0600

    UNFINISHED question23.c

--- question23.c
+++ question23.c
@@ -1,2 +1,23 @@
 //using a do-while loop, read integers until 0 is entered.
 // count positives, negatives, evens, and odds (excluding 0)
+
+#include <stdio.h>
+
+int main() {
+
+  int a;
+
+  printf("insert an integer(a) : ");
+  scanf("%d", &a);
+
+  do {
+    
+  }
+  while {
+    
+  }
+
+
+  
+return 0;
+}
