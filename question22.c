731c72d095d963fe2aadb0b829ff3dcab8f9087a
Christy Serna <christyserna04@gmail.com>
2025-11-14 22:38:11 -0600

    question22.c

--- question22.c
+++ question22.c
@@ -1,11 +1,18 @@
-//read a positive integer and reverse its digit using a while loop
-// show input & reversed number
+//create a program that reads a positive integer and reverse its digit using a while loop
+// also show input & reversed number
 
 #include <stdio.h>
 
 #int main() {
 
+int a;
 
+printf("insert a positive integer : ");
+scanf("%d", &a);
+
+while (a > 0) {
+  
+}
 
 
 return 0;
