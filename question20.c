eebfed117e0bd12e1c8c4f2b449025b7ea8ce132
Christy Serna <christyserna04@gmail.com>
2025-11-14 19:46:05 -0600

    UNFINISHED question20.c

---
+++ question20.c
@@ -0,0 +1,23 @@
+// use a do-while loop to repeatedly accept integers until the user enters 0.
+// after exit, print how many numbers were entered, excluding 0)
+
+#include <stdio.h>
+
+int main() {
+
+  int a;
+  
+  printf("insert an integer(a) : ");
+  scanf("%d", &a);
+
+  do {
+   
+  } 
+  while (a != 0) {
+    
+  };
+
+
+  
+return 0;
+}
