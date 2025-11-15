7c30c2360200de3ec9d8bf884f5cc38f8c589a55
Christy Serna <christyserna04@gmail.com>
2025-11-14 22:10:49 -0600

    question13.c

--- question13.c
+++ question13.c
@@ -5,15 +5,17 @@
 
 int main() {
 
-  int a, b, c;
+  int a;
 
   printf("insert an integer(a): ");
   scanf("%d", &a);
 
-  printf("insert an integer(b): ");
-  scanf("%d", &b);
-
-  
+  if ((a % 2 == 0 ) && ( a % 3 == 0 ) && (a % 7 != 0 )) {
+    printf("the integer you inserted is divisible by both 2 & 3, but not 7.");
+  }
+  else {
+    printf("the integer you inserted is NOT divisible by both 2 & 3, but not 7.");
+  }
 
   
 return 0;
