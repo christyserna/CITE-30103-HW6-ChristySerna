5eb6eccf5718c8b1e84c35006acb497aa5bf0360
Christy Serna <christyserna04@gmail.com>
2025-11-14 20:53:33 -0600

    question9.c

--- question9.c
+++ question9.c
@@ -1,2 +1,20 @@
 //write a short program that divides two integers and shows both integer
 // floating point division results using both implicit & explicit type casting
+
+include <stdio.h>
+
+int main() {
+
+int a = 10, b = 3;
+char x = 'a';
+double div;
+
+div = (double)a / b;
+
+x = x + 3;
+
+printf("implicit answer was %c", x);
+printf("explicit answer was %f", div); 
+  
+return 0;
+}
