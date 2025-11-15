902787d1099c4820df4931c9ec0e188e783cf9e2
Christy Serna <christyserna04@gmail.com>
2025-11-14 21:21:18 -0600

    question14.c

--- question14.c
+++ question14.c
@@ -5,7 +5,12 @@
 
 int main() {
 
+int a;
 
+  printf("insert an integer(a) : ");
+  scanf("%d", &a);
+  
+  (a > 0)? printf("the integer is positive") : ((a < 0)? printf("the integer is negative") : printf("the integer is zero"));
   
 
 return 0;
