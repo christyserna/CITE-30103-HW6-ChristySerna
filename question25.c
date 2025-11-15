d6c4b3386be87800a0454f194cddda6053441d81
Christy Serna <christyserna04@gmail.com>
2025-11-14 22:56:55 -0600

    Update question25.c

--- question25.c
+++ question25.c
@@ -1,2 +1,13 @@
 //using nested loops, print multiplication tables for a user-specified range (L to R). each table should
 // show products x1 to x10 with a blank line between tables
+
+include <stdio.h>
+
+#int main() {
+
+
+
+return 0; 
+}
+
+//this assignment was a lot harder than I expected, will study harder & do better next time.
