3bfdd3270f940a5c9e08ad18b8d4207df920d1c7
Christy Serna <christyserna04@gmail.com>
2025-11-14 21:38:56 -0600

    question18.c

--- question18.c
+++ question18.c
@@ -1,3 +1,48 @@
 // create a simple arithmetic calculator using switch. use the arithmetic
 // operators (* / - + ) as your case value & detect division by zero
 
+#include <stdio.h>
+
+int main() {
+  
+double a, b, result;
+char ch;
+
+  printf("enter *, /, -, + :\n");
+  scanf("%c", &ch);
+
+  printf("insert two numbers : ");
+  scanf(%d, %d, &a, &b);
+  
+switch (ch) {
+  
+case '*':
+result = a * b;
+printf("%d * %d = %d", a, b, result);
+break;
+
+case '/':
+if (b == 0) {
+break;
+}
+  
+  result = a / b;
+printf("%d / %d = %d", a, b, result);
+break;
+
+case '-':
+result = a - b;
+printf("%d - %d = %d", a, b, result);
+break;
+
+case '+':
+result = a + b;
+printf("%d + %d = %d", a, b, result);
+break;
+
+  default:
+  printf("the operator you chose is invalid. try again.");
+
+}
+  return 0;
+}
