d340f64c63793b4f72d3965d5ace382bc55c8fe6
Christy Serna <christyserna04@gmail.com>
2025-11-14 18:11:00 -0600

    UNIFNISHED question8.c

---
+++ question8.c
@@ -0,0 +1,18 @@
+// create a program that demonstrates global and local scope.
+// declare one variable outside of main and one inside of it
+// print both and explain their scope differences
+
+#include <stdio.h>
+
+int chocolatecake
+
+int main() {
+
+int vanillacake
+
+  ....
+
+  
+
+return 0;
+}
