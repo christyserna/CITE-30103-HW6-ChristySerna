8ae5d216071a8601b31602a8d15339d43287b614
Christy Serna <christyserna04@gmail.com>
2025-11-14 19:49:56 -0600

    UNFINISHED question24.c

---
+++ question24.c
@@ -0,0 +1,3 @@
+//write a program that prints numbers 1-50, skipping multiples of 5 (using continue) and
+//stopping completely when 40 is reached (using break)
+// print how many numbers were displayed
